d9d7b5fa7084a4a4176174a6a6ccd30d195deed4
nola360 <adaramolajr@gmail.com>
2020-01-31 15:41:14 -0500

    added character sets - brackets braces and parethesis

--- app_2.js
+++ app_2.js
@@ -2,7 +2,7 @@ let re;
 
 // Literal Character
 re = /hello/;
-re = /hello/i;
+re = /hello/i; // i  - case insensitive
 
 // MetaCharacheters Symbols
 re = /^h/i; //Must start with 'h' or else null output
@@ -14,10 +14,28 @@ re = /gre?a?y/i; //Optional Characters
 re = /gre?a?y\?/i; //Escape Characters
 
 
+//CHARACTER SETS
+// Bracekts [ ] - Character Sets
+re = /gr[ae]y/i; // Must be an a or an e
+re = /[GF]ray/i; // Must be an upperacase G or F
+re = /[^GF]ray/i; // Match anything except G or F
+re = /[A-Z]ray/; // Match anything uppercase letter A-Z
+re = /[A-Za-z]ray/; // Match both lowercase & uppercase letters A-Z
+re = /[0-99]ray/; // Match any digit
+re = /[0-9][0-99]ray/; // Match any digit
+
+
+// Braces {} - Quantifiers
+re = /Hel{2}o/i; //Must occur exactly {x} amount of times
+re = /Hel{2,4}o/i; //Must occur exactly {x} amount of times
+re = /Hel{2,}o/i; //Must occur at least {x} times
+
+//Parenthesis () - Grouping
+re = /^([0-9]x){3}$/ // Must match a # 3x only
 
 
 // String match
-const str = 'Gra4?';
+const str = '3x3x3x';
 
 
 // Log result
